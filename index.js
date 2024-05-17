fc9986e480c06a4279268b702f8839c0b29677a4
ODAKA Yukinari <s1320075@u-aizu.ac.jp>
2024-05-17 12:21:53 +0900

    complete

--- index.js
+++ index.js
@@ -17,8 +17,23 @@ if (c % 15 === 0){console.log("FizzBuzz")}else if
 (c % 3===0){console.log("Fizz")}
 else if (c % 5===0){console.log("Buzz")}
 
-const array =[1,8,3,2,5,6]
-switch(array){
+const array =[1,8,3,2,5,6];
+
     for (let i=0; i<array.length; i++)
-case''}
-(実装途中)
+{const year = parseInt(array[i]);
+
+switch (year){
+    case 1:console.log(array[i]+"is 1");
+    break;
+    case 2:console.log(array[i]+"is 2");
+    break;
+    case 3:console.log(array[i]+"is 3");
+    break;
+    case 4:console.log(array[i]+"is 4");
+    break;
+    default:console.log(array[i]+"is other grade");
+    break;
+}
+}
+
+
